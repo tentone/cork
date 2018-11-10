d11b11f6ba7bcb9edd7b7c7708ff1d235a00016a
Tentone <tentone@outlook.com>
2018-11-10 00:14:55 +0000

    Derp

--- main.cpp
+++ main.cpp
@@ -1,31 +1,46 @@
-#include <opencv2/opencv.hpp>
-#include <iostream>
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
 
 using namespace cv;
 using namespace std;
 
 int main(int argc, char** argv)
 {
-	// Read the image file
-	Mat image = imread("data/1.jpg");
+	const char* filename = argc >=2 ? argv[1] : "./data/1.jpg";
 
-	// Check for failure
-	if (image.empty()) 
+	// Loads an image
+	Mat src = imread(filename, IMREAD_COLOR);
+
+	// Check if image is loaded fine
+	if(src.empty())
 	{
-		cout << "Could not open or find the image" << endl;
-		cin.get(); //wait for any key press
+		printf(" Error opening image\n");
+		printf(" Program Arguments: [image_name -- default %s] \n", filename);
 		return -1;
 	}
 
-	String windowName = "Cork"; //Name of the window
+	Mat gray;
+	cvtColor(src, gray, COLOR_BGR2GRAY);
+	medianBlur(gray, gray, 5);
+	vector<Vec3f> circles;
+	HoughCircles(gray, circles, HOUGH_GRADIENT, 1,gray.rows/16,  // change this value to detect circles with different distances to each other
+				 100, 30, 1, 30 // change the last two parameters
+			// (min_radius & max_radius) to detect larger circles
+	);
 
-	namedWindow(windowName); // Create a window
+	for(size_t i = 0; i < circles.size(); i++)
+	{
+		Vec3i c = circles[i];
+		Point center = Point(c[0], c[1]);
+		// circle center
+		circle( src, center, 1, Scalar(0,100,100), 3, LINE_AA);
+		// circle outline
+		int radius = c[2];
+		circle( src, center, radius, Scalar(255,0,255), 3, LINE_AA);
+	}
 
-	imshow(windowName, image); // Show our image inside the created window.
-
-	waitKey(0); // Wait for any keystroke in the window
-
-	destroyWindow(windowName); //destroy the created window
-
+	imshow("Cork", src);
+	waitKey();
 	return 0;
 }
