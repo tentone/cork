594bf901b080e26f62b8eb86e7cc88523b95483a
Tentone <tentone@outlook.com>
2018-11-10 09:51:13 +0000

    Something

--- main.cpp
+++ main.cpp
@@ -1,38 +1,56 @@
+#include <iostream>
+#include <string>
+
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 
 #define WINDOW_NAME "Cork"
 
+#define KEY_ESC 27
+#define KEY_LEFT 81
+#define KEY_RIGHT 83
+
+#define IMAGES_COUNT 20
+
 using namespace cv;
 using namespace std;
 
 int main(int argc, char** argv)
 {
-	const char* file = "data/1.jpg";
+	VideoCapture cap(0);
+	Mat src;
 
-	//VideoCapture cap(0);
-	Mat src = imread(file, IMREAD_COLOR);
+	int image = 1;
 
-	//int width = cap.get(CAP_PROP_FRAME_WIDTH);
-	//int height = cap.get(CAP_PROP_FRAME_HEIGHT);
-	
 	//Prepare output window
 	namedWindow(WINDOW_NAME, WINDOW_AUTOSIZE);
-	//setMouseCallback(WINDOW_NAME, mouseEvents, &original);
-
-	/*if(cap.isOpened())
+	setMouseCallback(WINDOW_NAME, [](int event, int x, int y, int flags, void *param)
 	{
-		cap >> src;
-	}*/
+		//if(event == EVENT_LBUTTONDOWN){}
+		//else if(event == EVENT_RBUTTONDOWN)
+		//else if(event == EVENT_MBUTTONDOWN)
+		//else if(event == EVENT_MOUSEMOVE)
+	} , &src);
 
 	while(1)
 	{
+		if(cap.isOpened())
+		{
+			cap >> src;
+		}
+		else
+		{
+			src = imread("data/" + std::to_string(image) + ".jpg", IMREAD_COLOR);
+		}
+
 		Mat gray;
 		cvtColor(src, gray, COLOR_BGR2GRAY);
-		medianBlur(gray, gray, 5);
-		vector<Vec3f> circles;
 
+		imshow("Gray", gray);
+
+		//Detect circles
+		vector<Vec3f> circles;
 		HoughCircles(gray, circles, HOUGH_GRADIENT, 1, gray.rows / 16, 100, 30, 1, 100);
 
 		//Draw circle outline
@@ -54,26 +72,26 @@ int main(int argc, char** argv)
 		int key = waitKey(16);
 		if(key != -1)
 		{
-			if(key == 27)
+			if(key == KEY_ESC)
 			{
 				return 0;
 			}
+			else if(key == KEY_LEFT)
+			{
+				image--;
+				image = image < 1 ? image = IMAGES_COUNT : image;
+			}
+			else if(key == KEY_RIGHT)
+			{
+				image++;
+				image = image > IMAGES_COUNT ? image = 1 : image;
+			}
+			else
+			{
+				std::cout << key << std::endl;
+			}
 		}
-
-		/*if(cap.isOpened())
-		{
-			cap >> src;
-		}*/
 	}
 
 	return 0;
-}
-
-//Called on every mouse action
-void mouseEvents(int event, int x, int y, int flags, void *param)
-{
-	//if(event == EVENT_LBUTTONDOWN){}
-	//else if(event == EVENT_RBUTTONDOWN)
-	//else if(event == EVENT_MBUTTONDOWN)
-	//else if(event == EVENT_MOUSEMOVE)
 }
