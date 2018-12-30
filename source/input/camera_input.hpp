4d802be4208e21ba569bd9170038d6e75b7846cf
Tentone <tentone@outlook.com>
2018-12-30 23:40:47 +0000

    derp

--- source/input/camera_input.hpp
+++ source/input/camera_input.hpp
@@ -64,6 +64,70 @@ public:
 	}
 
 	/**
+	 * Callback called for new images by the internal appsink.
+	 *
+	 * Called from a TcamCamera object using the "set_new_frame_callback" method.
+	 */
+	static GstFlowReturn getFrameTcamCallback(GstAppSink *appsink, gpointer data)
+	{
+		int64 init = cv::getTickCount();
+
+		int width, height;
+		const GstStructure *str;
+
+		//Cast gpointer to ImageStatus*
+		ImageStatus *pdata = (ImageStatus*)data;
+		pdata->counter++;
+
+		//The following lines demonstrate, how to acces the image data in the GstSample.
+		GstSample *sample = gst_app_sink_pull_sample(appsink);
+		GstBuffer *buffer = gst_sample_get_buffer(sample);
+		GstMapInfo info;
+
+		gst_buffer_map(buffer, &info, GST_MAP_READ);
+		
+		if(info.data != NULL) 
+		{
+			//info.data contains the image data as blob of unsigned char 
+			GstCaps *caps = gst_sample_get_caps(sample);
+
+			//Get a string containg the pixel format, width and height of the image        
+			str = gst_caps_get_structure(caps, 0);    
+
+			if(strcmp(gst_structure_get_string(str, "format"), "BGRx") == 0)  
+			{
+				//Now query the width and height of the image
+				gst_structure_get_int(str, "width", &width);
+				gst_structure_get_int(str, "height", &height);
+
+				//Create a cv::Mat, copy image data into that and save the image.
+				pdata->frame.data = info.data;
+
+				resize(pdata->frame, pdata->resized, cv::Size(768, 480));
+					
+				//TODO <ADD CODE HERE>
+				//processFrame(pdata->resized);
+			}
+			else
+			{
+				std::cout << "Cork: Not the expected pixel format." << std::endl;
+			}
+		}
+		
+		//Clean up, unref and unmap buffers (to prevent leaks).
+		gst_buffer_unmap(buffer, &info);
+		gst_sample_unref(sample);
+
+		int64 end = cv::getTickCount();
+		double secs = (end - init) / cv::getTickFrequency();
+
+		std::cout << "Cork: Processing time was " << secs << " s." << std::endl;
+
+		//Set our flag of new image to true, so our main thread knows about a new image.
+		return GST_FLOW_OK;
+	}
+
+	/**
 	 * Put the calling thread to sleep for a bit. 
 	 */
 	static void wait()
