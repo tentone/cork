69253b45c2b1f170c020b688fcec5fcac4b97a42
Tentone <tentone@outlook.com>
2019-01-08 13:38:53 +0000

    Scripts

--- scripts/camera.sh
+++ scripts/camera.sh
@@ -20,16 +20,18 @@ cd ~
 
 #Build dependencies
 echo " - Get build dependencies"
-sudo apt-get install -y git g++ cmake pkg-config libudev-dev libudev1 libtinyxml-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libglib2.0-dev libgirepository1.0-dev libusb-1.0-0-dev libzip-dev uvcdynctrl python-setuptools libxml2-dev libpcap-dev libaudit-dev libnotify-dev autoconf intltool gtk-doc-tools python3-setuptools
+apt-get install -y git g++ cmake pkg-config libudev-dev libudev1 libtinyxml-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libglib2.0-dev libgirepository1.0-dev libusb-1.0-0-dev libzip-dev uvcdynctrl python-setuptools libxml2-dev libpcap-dev libaudit-dev libnotify-dev autoconf intltool gtk-doc-tools python3-setuptools
 
 #Runtime dependencies
 echo " - Get runtime dependencies"
-sudo apt-get install -y gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly libxml2 libpcap0.8 libaudit1 libnotify4 python3-pyqt5 python3-gi
+apt-get install -y gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly libxml2 libpcap0.8 libaudit1 libnotify4 python3-pyqt5 python3-gi
+apt-get install -y libglib2.0*
 
 #Clone repository
 echo " - Clone tiscamera repository"
 git clone --recursive https://github.com/TheImagingSource/tiscamera.git
 cd tiscamera
+rm -r build
 mkdir build
 cd build
 
@@ -49,10 +51,12 @@ cmake options
 #Installation target prefix (defaults to /usr/local)
 
 echo " - Build tiscamera"
-cmake -DBUILD_ARAVIS=ON -DBUILD_GST_1_0=ON -DBUILD_TOOLS=ON -DBUILD_V4L2=ON -DCMAKE_INSTALL_PREFIX=/usr ..
+cmake -DBUILD_ARAVIS=ON -DBUILD_GST_1_0=ON -DBUILD_TOOLS=ON -DBUILD_V4L2=ON -DCMAKE_INSTALL_PREFIX=/usr .. 
 make
-sudo make install
+make install
 
 #Without ARAVIS
 #cmake -DBUILD_ARAVIS=OFF -DBUILD_GST_1_0=ON -DBUILD_TOOLS=ON -DBUILD_V4L2=ON -DCMAKE_INSTALL_PREFIX=/usr ..
 
+#Copy missing header file
+cp /usr/lib/x86_64-linux-gnu/glib-2.0/include/glibconfig.h /usr/include/glib-2.0/glibconfig.h
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -16,38 +16,28 @@ fi
 
 #Update aptitude repository
 echo " - Update packages."
-#apt-get -qq update
+apt-get -qq update
 #apt-get -qq upgrade -y
 
 #Remove old versions
 #echo " - Remove old installations of x264."
 #apt-get -qq remove -y x264 libx264-dev
 
-#Install ROS
-echo " - Installing ROS"
-#sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-#apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-#apt-get -qq update
-#apt-get install ros-melodic-desktop-full
-
-echo " - Installing NodeJS"
-#apt-get -qq install nodejs npm
-
 #Intall dependencies
 echo " - Installing depedencies."
 
 #Build tools
-#apt-get -qq install -y build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
-#apt-get -qq install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
+apt-get -qq install -y build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+apt-get -qq install -y python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
 
 #Image IO
-#apt-get -qq install -y libjpeg8-dev libjasper-dev libpng12-dev
+apt-get -qq install -y libjpeg8-dev libjasper-dev libpng12-dev
 
 #Video IO
-#apt-get -qq install -y libjpeg-dev libpng-dev libtiff-dev libxvidcore-dev libx264-dev libavcodec-dev
-#apt-get -qq install -y libavformat-dev libswscale-dev libdc1394-22-dev libxine2-dev libv4l-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
-#apt-get -qq install -y libtbb-dev libfaac-dev libmp3lame-dev libtheora-dev install libvorbis-dev libopencore-amrnb-dev libopencore-amrwb-dev x264 v4l-utils
-#apt-get -qq install -y libtiff5-dev
+apt-get -qq install -y libjpeg-dev libpng-dev libtiff-dev libxvidcore-dev libx264-dev libavcodec-dev
+apt-get -qq install -y libavformat-dev libswscale-dev libdc1394-22-dev libxine2-dev libv4l-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
+apt-get -qq install -y libtbb-dev libfaac-dev libmp3lame-dev libtheora-dev install libvorbis-dev libopencore-amrnb-dev libopencore-amrwb-dev x264 v4l-utils
+apt-get -qq install -y libtiff5-dev
 
 #GKT for GUI
 #apt-get -qq install -y libgtk-3-dev qt5-default
@@ -56,8 +46,8 @@ echo " - Installing depedencies."
 #apt-get -qq install -y libatlas-base-dev gfortran
 
 #Go to home
-echo " - Navigate to home"
-cd ~
+#echo " - Navigate to home"
+#cd ~
 
 #Python 3 support
 #echo " - Install Python 3 support"
@@ -72,15 +62,15 @@ cd ~
 #mkvirtualenv cv -p python3
 
 #Get the openCV code
-echo " - Clone opencv repository"
-git clone https://github.com/opencv/opencv.git
-git clone https://github.com/opencv/opencv_contrib.git
+#echo " - Clone opencv repository"
+#git clone https://github.com/opencv/opencv.git
+#git clone https://github.com/opencv/opencv_contrib.git
 
 #Build opencv
-echo " - Build opencv"
-cd opencv
-mkdir build
-cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_V4L=ON -D INSTALL_PYTHON_EXAMPLES=O -d WITH_OPENGL=ON -D WITH_QT=ON -D WITH_OPENCL=ON -D INSTALL_C_EXAMPLES=ON -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules -D PYTHON_EXECUTABLE=~/.virtualenvs/cv/bin/python -D BUILD_EXAMPLES=ON ..
-make -j4
-make install
+#echo " - Build opencv"
+#cd opencv
+#mkdir build
+#cd build
+#cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_V4L=ON -D INSTALL_PYTHON_EXAMPLES=O -d WITH_OPENGL=ON -D WITH_QT=ON -D WITH_OPENCL=ON -D INSTALL_C_EXAMPLES=ON -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules -D PYTHON_EXECUTABLE=~/.virtualenvs/cv/bin/python -D BUILD_EXAMPLES=ON ..
+#make -j4
+#make install
